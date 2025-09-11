e4e64cd77bcdcebfc8e041c3d4b4c116a69d7dd3
Mark Golla <Mark.Golla@dell.com>
2025-09-11 11:16:02 -0500

    Add support for Hub, Fan, and Air Temp cards in setenv.sh
    Updated setenv.sh to handle multiple FT260 devices, specifically Hub, Fan Card, and Air Temperature Card, by searching for their respective PVIDs and exporting their sysfs paths. Improved usage examples and variable declarations for clarity and expanded device support.

--- setenv.sh
+++ setenv.sh
@@ -9,28 +9,55 @@
 # Usage:
 #   . ./setenv.sh
 
-# Example with 1 ft260 device:
-: '
-$ . setenv.sh
-sysfs_i2c_14
-sysfs_ttyFT0
-
-$ echo $sysfs_i2c_14
-/sys/bus/hid/drivers/hid-generic/0003:413c:d100.0011
-'
-# Example with 2 ft260 devices:
+# Example with Hub, Fan, and Air Temperature cards:
 : '
 $ . setenv.sh
 sysfs_i2c_15
-sysfs_ttyFT1
-sysfs_i2c_14
 sysfs_ttyFT0
+sysfs_i2c_16
+sysfs_ttyFT1
+sysfs_i2c_17
 
-$ echo $sysfs_i2c_15
-/sys/bus/hid/drivers/hid-generic/0003:413c:d100.0013
+$ echo $sysfs_i2c_15   # Hub I2C
+/sys/bus/hid/drivers/ft260/0003:413C:D100.0001
+
+$ echo $sysfs_i2c_16   # Fan Card I2C
+/sys/bus/hid/drivers/ft260/0003:413C:D101.0003
+
+$ echo $sysfs_i2c_17   # Air Temperature Card I2C
+/sys/bus/hid/drivers/ft260/0003:413C:D104.0015
 '
 
-PVID='*413c:d100*'
+# Hub
+PVID='*413C:D100*'
+SYSFS_FT260=/sys/bus/hid/drivers/ft260/
+
+a=$(find $SYSFS_FT260 -name $PVID | xargs -I % sh -c 'find %/ -maxdepth 1 -name i2c-* -o -name tty*')
+for w in $a; do
+	b=$(basename "$w")
+	[ "$b" = "tty" ] && e=$(find $w/ -name ttyF*) && b=$(basename "$e")
+	c=$(dirname "$w")
+	d=sysfs_${b/-/_}
+	echo $d
+	declare $d=$c
+done
+
+# Fan Card
+PVID='*413C:D101*'
+SYSFS_FT260=/sys/bus/hid/drivers/ft260/
+
+a=$(find $SYSFS_FT260 -name $PVID | xargs -I % sh -c 'find %/ -maxdepth 1 -name i2c-* -o -name tty*')
+for w in $a; do
+	b=$(basename "$w")
+	[ "$b" = "tty" ] && e=$(find $w/ -name ttyF*) && b=$(basename "$e")
+	c=$(dirname "$w")
+	d=sysfs_${b/-/_}
+	echo $d
+	declare $d=$c
+done
+
+# Air Temperature Card
+PVID='*413C:D104*'
 SYSFS_FT260=/sys/bus/hid/drivers/ft260/
 
 a=$(find $SYSFS_FT260 -name $PVID | xargs -I % sh -c 'find %/ -maxdepth 1 -name i2c-* -o -name tty*')
